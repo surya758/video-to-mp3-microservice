73f3dc59f07e75bea1ac662f904db3575ab315a7
Suryakant <skant758@gmail.com>
2024-05-25 18:18:12 +0530

    wrote validation route

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -16,7 +16,7 @@ app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-app.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+app.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 app.config["DEBUG"] = True
 
 
@@ -26,21 +26,54 @@ def login():
     password = request.json.get("password")
     if not (email and password):
         return "Missing credentials", 401
-
     # Check if user exists in the database
     cur = mysql.connection.cursor()
-    res = cur.request("SELECT email, password FROM user WHERE email=%s", (email))
+    res = cur.execute("SELECT email, password FROM user WHERE email=%s", (email,))
 
     if res > 0:
-        user_row = cur.fetchOne()
+        user_row = cur.fetchone()
         email_from_db = user_row[0]
         password_from_db = user_row[1]
         if email != email_from_db or password != password_from_db:
             return "Invalid credentials", 401
         else:
-            createJWT(email, os.environ.get("JWT_SECRET"), true)
+            return createJWT(email, os.environ.get("JWT_SECRET"), True)
     else:
         return "User doesn't exist", 401
+
+
+@app.route("/validate", methods=["POST"])
+def validate():
+    auth_header = request.headers["Authorization"]
+    if not auth_header:
+        return "Missing credentials", 401
+
+    parts = auth_header.split()
+    if len(parts) != 2 or parts[0].lower() != "bearer":
+        return "Invalid token format", 401
+
+    jwt_token = parts[1]
+    try:
+        decoded_jwt = jwt.decode(
+            jwt_token, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except Exception as e:
+        return f"Not authorized: {str(e)}", 401
+
+    return decoded_jwt, 200
+
+
+def createJWT(email, secret, authz):
+    return jwt.encode(
+        {
+            "email": email,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "isAdmin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
 
 
 if __name__ == "__main__":

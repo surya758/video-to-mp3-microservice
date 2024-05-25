4c3e9c7b1544150899cab9d2308ca145fd297cfa
Suryakant <skant758@gmail.com>
2024-05-25 17:25:25 +0530

    setup login route

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -3,11 +3,13 @@ import datetime
 import jwt
 from flask import Flask, request
 from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 mysql = MySQL(app)
+bcrypt = Bcrypt(app)
 
 # config
 app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
@@ -20,12 +22,25 @@ app.config["DEBUG"] = True
 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json["username"]
-    password = request.json["password"]
-    if not username or password:
-        return "missing credentials", 404
+    email = request.json.get("email")
+    password = request.json.get("password")
+    if not (email and password):
+        return "Missing credentials", 401
 
     # Check if user exists in the database
+    cur = mysql.connection.cursor()
+    res = cur.request("SELECT email, password FROM user WHERE email=%s", (email))
+
+    if res > 0:
+        user_row = cur.fetchOne()
+        email_from_db = user_row[0]
+        password_from_db = user_row[1]
+        if email != email_from_db or password != password_from_db:
+            return "Invalid credentials", 401
+        else:
+            createJWT(email, os.environ.get("JWT_SECRET"), true)
+    else:
+        return "User doesn't exist", 401
 
 
 if __name__ == "__main__":
